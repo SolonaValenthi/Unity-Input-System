3d62f6d396744d9f41d365389c80dfa66deb2440
Nikolai Nelson <nikolainelson@comcast.net>
2024-01-18 03:39:26 -0800

    input script setup

---
+++ Assets/Inputs/PlayerInput.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerInput : MonoBehaviour
+{
+    private PlayerInputActions _input;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _input = new PlayerInputActions();
+        _input.Dog.Enable();
+        _input.Dog.Bark.performed += Bark_performed;
+    }
+
+    private void Bark_performed(InputAction.CallbackContext context)
+    {
+        Debug.Log("Bark");
+    }
+}
