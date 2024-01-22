2e8f3e45574c3592069673784e6c32831aff30a6
Nikolai Nelson <nikolainelson@comcast.net>
2024-01-22 05:28:36 -0800

    jump charge meter setup

--- Assets/Inputs/Sphere.cs
+++ Assets/Inputs/Sphere.cs
@@ -1,12 +1,15 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.UI;
 
 public class Sphere : MonoBehaviour
 {
     private SphereInputActions _input;
 
     [SerializeField] private float _jumpForce;
+    [SerializeField] private Slider _jumpMeter;
 
     Rigidbody _rb;
 
@@ -20,7 +23,7 @@ public class Sphere : MonoBehaviour
         _rb = GetComponent<Rigidbody>();
     }
 
-    private void Jump_canceled(UnityEngine.InputSystem.InputAction.CallbackContext context)
+    private void Jump_canceled(InputAction.CallbackContext context)
     {
         float forceOnHold = (float)context.duration;
         forceOnHold = Mathf.Clamp(forceOnHold, 0.5f, 1.0f);
@@ -30,6 +33,18 @@ public class Sphere : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        ChargeMeter();
+    }
+
+    private void ChargeMeter()
+    {
+        if (_input.Sphere.Jump.IsPressed())
+        {
+            _jumpMeter.value += Time.deltaTime;
+        }
+        else if (_jumpMeter.value > 0f)
+        {
+            _jumpMeter.value -= 0.02f;
+        }
     }
 }
